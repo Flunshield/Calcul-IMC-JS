0a6ac4d8bf571648e5d49bb55f20200b5ca2abb8
Julien BERTRAND <j.bertrand.sin@gmail.com>
2023-03-15 21:37:08 +0100

    fix: refacto

--- Challenge IMC/test.js
+++ Challenge IMC/test.js
@@ -1,25 +1,14 @@
 function IMC() 
     {
-
         // Sélectionner l'élément input et récupérer sa valeur
         let size = document.getElementById("taille").value;
         let lenght = document.getElementById("poids").value;
-        console.log('test');
-        console.log(size);
-        console.log('test2');
-        console.log(lenght);
-        console.log('test3');
-        result = lenght/(size*size);
-        console.log('test4');
-        let res = result.toFixed(1);
-        test = "Votre IMc est de : " + res;
-
-
+        res = lenght/(size*size);
+        res = res.toFixed(1);
+        result = "Votre IMc est de : " + res;
         let k = document.getElementById("input1");
-
-        k.innerHTML = "</br>" + test;
-        
-    };
+        k.innerHTML = "</br>" + result; 
+    }
 
     function changeColor() { 
         color = document.getElementById("couleur").value;
